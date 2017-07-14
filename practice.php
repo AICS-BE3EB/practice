78383adfb5d829d6b4e0241b44e04d9129b75380
rickyx12 <aizekrysler@gmail.com>
2017-07-15 01:22:21 +0800

    add her name on top of the list

--- practice.php
+++ practice.php
@@ -6,10 +6,10 @@
 </head>
 <body>
 	<ul>
+		<li>Lovelyn Grace Lebrado <3 <3 heart heart lol </li>
 		<li>Ricardo Osit</li>
         <li>Jm Regino</li>
         <li>Jewellyn Sambayan</li>
-        <li>Lovelyn Grace Lebrado <3 <3 heart heart lol </li>
 	</ul>
 </body>
 </html>
