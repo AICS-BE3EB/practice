ab11183cb2ca3c9752fab2bf2961f689e7b57908
Spencer Bytes <spencerbytes.exe@gmail.com>
2017-07-15 01:49:51 +0800

    Test Form Added

--- practice.php
+++ practice.php
@@ -1,16 +1,73 @@
+
+BETAPHP FormatterHomeFeaturesContactAbout
+Bookmark and Share 
+
+Welcome to the new PHP Formatter BETA!
+We've given PHP Formatter a new design as well as a new engine! The new engine features:
+Blazingly fast, on the fly formatting of all scripts!
+PHP 4 and PHP 5 support
+Handy syntax check function
+Ability to create your own coding styles, or to use builtin styles
+Proper handling of doc comments, and alternative control structures
+
+
+InputStyleFormat
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
   <title></title>
 </head>
-<body>
-	<ul>
-		<li>Lovelyn Grace Lebrado</li>
-		<li>Ricardo Osit</li>
-        <li>Jm Regino</li>
-        <li>Jewellyn Sambayan</li>
-        <p> walang forever </p>
-	</ul>
-</body>
+	<body>
+    	<ul>
+        	<li>Lovelyn Grace Lebrado</li>
+        	<li>Ricardo Osit</li>
+        	<li>Jm Regino</li>
+        	<li>Jewellyn Sambayan</li>
+    	</ul>
+        <form action="/action_page.php"> <!--TEST ONLY-->
+  			<div class="container">
+    			<label><b>Username</b></label>
+   				<input type="text" placeholder="Enter Username" name="uname" required>
+
+ 		   		<label><b>Password</b></label>
+    			<input type="password" placeholder="Enter Password" name="psw" required>
+
+  		  		<button type="submit">Login</button>	
+ 			 </div>
+ 		</form> 
+	</body>
 </html>
