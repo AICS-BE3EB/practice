21676ed6e31532e18f34499c74f771f815b4b0ca
dadada-PC\dadada <jm.regino123@gmail.com>
2017-07-15 01:53:14 -0800

    table.php

---
+++ table.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Table</title>
+
+</head>
+<body>
+	<div class="table">
+	<table border="4">
+		<thead>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Age</th>
+			<th>student no.</th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Jm</td>
+				<td>Regino</td>
+				<td>20</td>
+				<td>000001</td>
+			</tr>
+			<tr>
+				<td>Jewel</td>
+				<td>Sambayan</td>
+				<td>25</td>
+				<td>000002</td>
+			</tr>
+			<tr>
+				<td>Ricardo</td>
+				<td>Osit</td>
+				<td>30</td>
+				<td>000003</td>
+			</tr>
+			<tr>
+				<td>Lovelyn Grace</td>
+				<td>Lebrado</td>
+				<td>20</td>
+				<td>000004</td>
+			</tr>
+			<tr>
+				<td>Johnny</td>
+				<td>Montero</td>
+				<td>29</td>
+				<td>000005</td>
+			</tr>
+		</tbody>
+	</div>
+</table>
+</body>
+</html>
